f80db72ad38ef149123cf8f1dac3f15e464eddbf
Matthieu El Hakim <matthieuelhakim@yahoo.fr>
2023-07-12 17:23:03 +0200

    Ajout de l'api

---
+++ app.py
@@ -0,0 +1,138 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  6 11:08:41 2023
+
+@author: matth
+"""
+
+import pandas as pd
+import streamlit as st
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from joblib import load
+import shap
+from flask import Flask, jsonify, request
+import json
+
+
+shap.initjs()
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+# Chargement des fichiers
+data_test = pd.read_csv('./donnees_test_essai.csv')
+data_train = pd.read_csv('./donnees_train_essai.csv')
+noms_col = data_test.columns
+
+# Séparation des groupes
+group_0 = data_train[data_train['TARGET'] == 0]
+group_1 = data_train[data_train['TARGET'] == 1]
+
+# Chargement du modèle
+model = load('best_model.joblib')
+
+# Chargement des valeurs Shap
+explainer = shap.TreeExplainer(model)
+def load_shap_values(data):
+    shap_values = explainer(data)
+    return shap_values
+
+shap_values_test = load_shap_values(data_test)
+shap_values_train_group_0 = load_shap_values(group_0.drop('TARGET',axis=1))
+#shap_values_train_group_1 = load_shap_values(group_1.drop('TARGET',axis=1))
+
+# Chargement des définitions des features
+df_expli = pd.read_csv('./HomeCredit_columns_description.csv', encoding = 'latin-1')
+   
+# Noms des features
+liste_features = data_test.columns.tolist()
+
+# Liste des identifiants clients
+liste_id = data_test['SK_ID_CURR'].to_list()
+
+
+# Renvoie la liste des identifiants
+@app.route('/credit', methods = ['GET'])
+def liste_identifiants():
+    return jsonify(liste_id)
+
+# Renvoie la liste des features
+@app.route('/credit/liste_feature', methods = ['GET'])
+def liste_feature():
+    return sorted(liste_features)
+
+# Renvoie la probabilité que le client soit sans risque
+@app.route('/credit/<id_client>', methods = ['GET'])
+def credit_client(id_client):
+    data_client = data_test.loc[data_test['SK_ID_CURR']== int(id_client)] 
+    proba = model.predict_proba(data_client) # Calcul de la probabilité d'obtenir 0
+    proba_0 = round(proba[0][0]*100)
+    resultats = {}
+    resultats['proba'] = proba_0    
+    return jsonify(resultats)
+
+# Renvoie les données du client
+@app.route('/credit/<id_client>/data', methods = ['GET'])
+def donnees_client(id_client):
+    data_client = data_test.loc[data_test['SK_ID_CURR']== int(id_client)]
+    resultats = {}
+    resultats['data'] = data_client.to_json(orient='records')
+    return jsonify(resultats)
+
+# Influence globale des features
+@app.route('/credit/globale', methods = ['GET'])
+def globale():
+    shap_val = explainer.shap_values(data_train.drop('TARGET',axis=1))
+    return {'shap_values_0': shap_val[0].tolist(),
+            'shap_values_1': shap_val[1].tolist(),
+            'liste_features': liste_features
+            }
+
+# Valeurs shap des données client    
+@app.route('/credit/locale/<int:id_client>', methods = ['GET'])
+def valeurs_shap(id_client):
+    data_client = data_test.loc[data_test['SK_ID_CURR']== int(id_client)]
+    index = data_client.index
+    shap_values = shap_values_test[index]
+    shap_val = shap_values.values.tolist()
+    shap_base = shap_values.base_values.tolist()
+    shap_data = shap_values.data.tolist()
+    return {
+        'shap_val': shap_val,
+        'shap_base': shap_base,
+        'shap_data': shap_data,
+        'feature_names': liste_features
+            }
+    
+# Valeurs shap du data_train_group_0 (clients sans risque)
+@app.route('/credit/moyenne', methods = ['GET'])
+def moyenne():
+    shap_val = explainer.shap_values(group_0.drop('TARGET',axis=1))
+    return {'shap_values_0': shap_val[0].tolist(),
+            'shap_values_1': shap_val[1].tolist(),
+            'liste_features': liste_features
+            }
+    
+# Explications des features
+@app.route('/credit/descriptions', methods = ['GET'])
+def descriptions():
+    return list(set(sorted(df_expli['Row'].tolist())))
+
+@app.route('/credit/descriptions/<feature>', methods = ['GET'])
+def textes(feature):
+    texte = df_expli.loc[df_expli['Row'] == feature, 'Description'].tolist()
+    return jsonify(texte)
+
+# Renvoie les valeurs des features pour les graphiques
+@app.route('/credit/nuage/<feature>/<id_client>', methods = ['GET'])
+def nuage(feature, id_client):
+    data_client = data_test.loc[data_test['SK_ID_CURR']== int(id_client)]
+    return {'group_0': group_0[feature].tolist(),
+            'group_1': group_1[feature].tolist(),
+            'client': data_client[feature].tolist()
+             }
+
+if __name__ == '__main__':
+    app.run(debug=True)
